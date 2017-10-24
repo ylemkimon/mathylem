329e75f2d23313fa00eb1fa3f090c91d2b024a93
ylemkimon <ylemkimon@naver.com>
2017-10-24 17:32:06 +0900

    Improve Windows make script

--- make.bat
+++ make.bat
@@ -1,5 +1,6 @@
 @echo off
-.\node_modules\.bin\browserify.cmd src/mathylem.js -o build/mathylem.min.js --standalone MathYlem -d
-.\node_modules\.bin\browserify.cmd src/mathylem_render.js -o build/mathylem_render.min.js --standalone mathylem_render -d
-.\node_modules\.bin\browserify.cmd src/mathylem_doc.js -o build/mathylem_doc.min.js --standalone MathYlemDoc -d
-.\node_modules\.bin\browserify.cmd src/mathylem_backend.js -o build/mathylem_backend.min.js --standalone MathYlemBackend -d
+call .\node_modules\.bin\browserify.cmd src/mathylem.js -o build/mathylem.min.js --standalone MathYlem -d
+call .\node_modules\.bin\browserify.cmd src/mathylem_render.js -o build/mathylem_render.min.js --standalone mathylem_render -d
+call .\node_modules\.bin\browserify.cmd src/mathylem_doc.js -o build/mathylem_doc.min.js --standalone MathYlemDoc -d
+call .\node_modules\.bin\browserify.cmd src/mathylem_backend.js -o build/mathylem_backend.min.js --standalone MathYlemBackend -d
+pause
