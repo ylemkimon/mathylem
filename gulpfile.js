78f0ca7f995fcb924ac6179a1c76b17217df320c
ylemkimon <ylemkimon@naver.com>
2017-12-20 01:46:15 +0900

    Cleanup gulpfile.js
    - Lint gulpfile.js
    - Use .babelrc

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,58 @@
-var browserify = require('browserify');
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var merge = require('merge-stream');
-var sass = require('gulp-sass');
-var prefix = require('gulp-autoprefixer');
-var cleancss = require('gulp-clean-css');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
-var gutil = require('gulp-util');
-var sourcemaps = require('gulp-sourcemaps');
+/* eslint no-console: "off", no-underscore-dangle: "off", camelcase: "off" */
+const browserify = require('browserify');
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const merge = require('merge-stream');
+const sass = require('gulp-sass');
+const prefix = require('gulp-autoprefixer');
+const cleancss = require('gulp-clean-css');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const sourcemaps = require('gulp-sourcemaps');
 
-var BUILD = 'build/';
+const BUILD = 'build/';
 
-gulp.task('js', function () {
-  return browserify({
-    entries: ['./js/mathylem.js'],
-    standalone: 'MathYlem'
-  }).bundle()
-    .on('error', gutil.log)
-    .pipe(source('mathylem.js'))
-    .pipe(buffer())
-    .pipe(gulp.dest(BUILD))
-    .pipe(rename({ extname: '.min.js' }))
-    .pipe(uglify({
-      output: {
-        ascii_only: true
-      }
-    }))
-    .pipe(gulp.dest(BUILD));
-});
-
-gulp.task('js-debug', function () {
-  return browserify({
-    entries: ['./js/mathylem.js'],
+function buildJS(debug) {
+  let stream = browserify({
+    transform: 'babelify',
+    entries: ['js/mathylem.js'],
     standalone: 'MathYlem',
-    debug: true
+    debug,
   }).bundle()
-    .on('error', gutil.log)
-    .pipe(source('mathylem.min.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(uglify({
-      output: {
-        ascii_only: true
-      }
-    }))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(BUILD));
-});
+    .pipe(source(debug ? 'mathylem.min.js' : 'mathylem.js'))
+    .pipe(buffer());
 
-gulp.task('css', function () {
-  var scss = gulp.src('./css/*.scss')
-    .on('error', gutil.log)
+  if (debug) {
+    stream = stream.pipe(sourcemaps.init({ loadMaps: true }));
+  } else {
+    stream = stream.pipe(gulp.dest(BUILD))
+      .pipe(rename({ extname: '.min.js' }));
+  }
+
+  stream = stream.pipe(uglify({
+    output: {
+      ascii_only: true,
+    },
+  }));
+
+  if (debug) {
+    stream = stream.pipe(sourcemaps.write('./'));
+  }
+
+  return stream.pipe(gulp.dest(BUILD));
+}
+
+gulp.task('js', () => buildJS(false));
+gulp.task('js-debug', () => buildJS(true));
+
+gulp.task('css', () => {
+  const scss = gulp.src('css/*.scss')
     .pipe(sass())
     .pipe(prefix());
 
-  return merge(scss, gulp.src([
-      'node_modules/katex/static/katex.css', 'css/*.css']))
+  return merge(scss, gulp.src(['node_modules/katex/static/katex.css', 'css/*.css']))
     .pipe(concat('mathylem.css'))
     .pipe(gulp.dest(BUILD))
     .pipe(rename({ extname: '.min.css' }))
@@ -66,9 +60,7 @@ gulp.task('css', function () {
     .pipe(gulp.dest(BUILD));
 });
 
-gulp.task('fonts', function () {
-  return gulp.src(['lib/katex/static/fonts/*', 'css/fonts/*'])
-    .pipe(gulp.dest(BUILD + 'fonts/'));
-});
+gulp.task('fonts', () => gulp.src(['lib/katex/static/fonts/*', 'css/fonts/*'])
+  .pipe(gulp.dest(`${BUILD}fonts/`)));
 
 gulp.task('default', ['js', 'css', 'fonts']);
